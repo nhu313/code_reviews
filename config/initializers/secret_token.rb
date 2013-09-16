0ebe437247f3406a10531111650dcbfaf252e070
Nhu Nguyen <nhu313@gmail.com>
2013-09-16 08:46:03 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeReview::Application.config.secret_key_base = 'ac803be58c1124fdbf461b1298b3ae10c9a1fbe5397ffbb349fcb39259dbc277564b7e5f0bd410c1acec5b81c7313b0d249f4eb7783ee47f001c45115f42278f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeReview::Application.config.session_store :cookie_store, key: '_code_review_session'
