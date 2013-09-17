03b8b1cec25966c4a9b0d081e976eaffe7b9e1a5
Nhu Nguyen <nhu313@gmail.com>
2013-09-17 10:25:56 -0500

    changed the way to get google id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  config = YAML.load_file(Rails.root.join('config/omniauth.yml'))[Rails.env]
-  provider :google_oauth2, config['google_id'], config['google_secret'], {approval_prompt: ''}
+  omniauth_config = Rails.root.join('config/omniauth.yml')
+
+  if File.exists?(omniauth_config)
+    config = YAML.load_file(omniauth_config)[Rails.env]
+  else
+    config = ENV
+  end
+
+  provider :google_oauth2, ENV['google_id'], ENV['google_secret'], {approval_prompt: ''}
 end
