a20b939137c9e901c27557ea4449f0b42e858baa
Nhu Nguyen <nhu313@gmail.com>
2013-09-20 10:27:36 -0500

    cleaned up request

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,6 @@ class ReviewsController < ApplicationController
 
   def index
     if session[:user_id]
-      @requests = Request.all
       render action: "index"
     else
       redirect_to signin_url
