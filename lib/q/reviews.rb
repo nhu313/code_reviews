9fd6ce5cbf5e42dc8d7c7f1defc82dce80910246
Nhu Nguyen <nhu313@gmail.com>
2013-10-11 09:53:52 -0500

    added reviews

--- lib/q/reviews.rb
+++ lib/q/reviews.rb
@@ -1,5 +1,28 @@
+require 'q/db_service'
+
 module Q
   class Reviews
+
+    def self.for(user_id)
+      Reviews.new(user_id)
+    end
+
+    def initialize(user_id)
+      @user_id = user_id
+      @request_db = DBService.create(:review_request)
+      @reply_db = DBService.create(:review_reply)
+    end
+
+    def requested
+      request_db.find_all({:user_id => user_id, :archive => false})
+    end
+
+    def taken
+      reply_db.find_all({:reviewer_id => user_id, :posted_date => nil})
+    end
+
+    private
+    attr_reader :user_id, :request_db, :reply_db
 
   end
 end
--- spec/q/reviews_spec.rb
+++ spec/q/reviews_spec.rb
@@ -3,6 +3,25 @@ require 'q/reviews'
 
 module Q
   describe Reviews do
+    let(:user_id) {5444}
+    let(:reviews) {Reviews.for(user_id)}
+    let(:data) {["1", "2"]}
+
+    it "has user requested review" do
+      request_model = Models[:review_request]
+      request_model.data = data
+
+      reviews.requested.should == data
+      request_model.filter.should == {:user_id => user_id, :archive => false}
+    end
+
+    it "has user taken review" do
+      reply_model = Models[:review_reply]
+      reply_model.data = data
+
+      reviews.taken.should == data
+      reply_model.filter.should == {:reviewer_id => user_id, :posted_date => nil}
+    end
 
   end
 end
