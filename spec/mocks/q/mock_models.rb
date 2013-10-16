73d18f63651dfcbf1fc0357b6d051d5948cbb1dd
Nhu Nguyen <nhu313@gmail.com>
2013-10-16 18:56:04 -0500

    changed mock models

--- spec/mocks/q/mock_models.rb
+++ spec/mocks/q/mock_models.rb
@@ -1,10 +1,15 @@
 require 'mocks/q/mock_model'
 
 module Q
-  MockModels =
+  class MockModels
+    def self.map
+      Hash.new(MockModel.new)
       # set default and add models
-      {:review_request => MockModel.new,
-        :review_reply => MockModel.new,
-        :skipped_review_request => MockModel.new,
-        :user => MockModel.new}
+      # {:review_request => MockModel.new,
+      #   :review_reply => MockModel.new,
+      #   :skipped_review_request => MockModel.new,
+      #   :user => MockModel.new
+      # }
+    end
+  end
 end
